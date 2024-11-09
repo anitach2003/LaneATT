3546e7e8809e642da7e67a9c94a93af2f3ff22c1
anitach2003 <123134603+anitach2003@users.noreply.github.com>
2024-11-09 15:16:51 +0330

    Update laneatt.py

--- lib/models/laneatt.py
+++ lib/models/laneatt.py
@@ -418,14 +418,14 @@ class LaneATT(nn.Module):
             iou_loss += iou_loss + liou_loss(
                     reg_pred, reg_target,
                     self.img_w, length=15)
-            reg_loss += smooth_l1_loss(reg_pred, reg_target)
+          #  reg_loss += smooth_l1_loss(reg_pred, reg_target)
             cls_loss += focal_loss(cls_pred, cls_target).sum() / num_positives
 
         # Batch mean
         cls_loss /= valid_imgs
-        reg_loss /= valid_imgs
+       # reg_loss /= valid_imgs
 
-        loss = cls_loss_weight * cls_loss + reg_loss+iou_loss
+        loss = cls_loss_weight * cls_loss +iou_loss
         return loss, {'cls_loss': cls_loss, 'reg_loss': iou_loss, 'batch_positives': total_positives}
 
     def compute_anchor_cut_indices(self, n_fmaps, fmaps_w, fmaps_h):
