4004135dab686b413b7fba631fbe4a3b636ebee9
anitach2003 <123134603+anitach2003@users.noreply.github.com>
2024-11-25 00:50:21 +0330

    Update laneatt.py

--- lib/models/laneatt.py
+++ lib/models/laneatt.py
@@ -15,7 +15,7 @@ from .resnet import resnet122 as resnet122_cifar
 from .matching import match_proposals_with_targets
 from .conv import CSPStage
 import torch
-
+from main_model import main_model
 
 def line_iou(pred, target, img_w, length=15, aligned=True):
     '''
@@ -212,7 +212,7 @@ class LaneATT(nn.Module):
                  pretrained_backbone=True,
                  S=72,
                  img_w=640,
-                 img_h=360,
+                 img_h=384,
                  anchors_freq_path=None,
                  topk_anchors=None,
                  anchor_feat_channels=64):
@@ -267,23 +267,24 @@ class LaneATT(nn.Module):
         self.initialize_layer(self.conv1)
         self.initialize_layer(self.cls_layer)
         self.initialize_layer(self.reg_layer)
+        self.main=main_model(1).to('cuda')
       #  self.resnet=resnet_fpn_backbone(backbone_name='resnet18', pretrained=True).to('cuda')
     def forward(self, x, conf_threshold=None, nms_thres=0, nms_topk=3000):
         
-        batch_features = self.feature_extractor(x)
+        batch_features = self.main(x)
        # A=CSPStage(512,64,4,spp=True).to('cuda')
        # batch_features=A(batch_features)
         #model = SwinFeatureExtractor()
        # batch_features=model(x)
 
        # bach=CBAM(512,512).to('cuda')
-        batch_features=self.conv1(batch_features)
+        #batch_features=self.conv1(batch_features)
         
         batch_anchor_features = self.cut_anchor_features(batch_features)
 
         # Join proposals from all images into a single proposals features batch
         batch_anchor_features = batch_anchor_features.view(-1, self.anchor_feat_channels * self.fmap_h)
-
+        
         # Add attention features
         batch_size = x.shape[0]
         num_proposals = len(self.anchors)
@@ -296,11 +297,18 @@ class LaneATT(nn.Module):
         num_layers = 6
 
 
-        anchor_features = batch_anchor_features.view(batch_size, num_proposals, d_k)
+       # anchor_features = batch_anchor_features.view(batch_size, num_proposals, d_k)
 
 
-        transformer_model = TransformerModel(input_dim, num_heads, hidden_dim, num_layers).to('cuda')
-        attention_matrix = transformer_model(anchor_features)
+       # transformer_model = TransformerModel(input_dim, num_heads, hidden_dim, num_layers).to('cuda')
+       # attention_matrix = transformer_model(anchor_features)
+        softmax = nn.Softmax(dim=1)
+        scores = self.attention_layer(batch_anchor_features)
+        attention = softmax(scores).reshape(x.shape[0], len(self.anchors), -1)
+        attention_matrix = torch.eye(attention.shape[1], device=x.device).repeat(x.shape[0], 1, 1)
+        non_diag_inds = torch.nonzero(attention_matrix == 0., as_tuple=False)
+        attention_matrix[:] = 0
+        attention_matrix[non_diag_inds[:, 0], non_diag_inds[:, 1], non_diag_inds[:, 2]] = attention.flatten()
         batch_anchor_features = batch_anchor_features.reshape(x.shape[0], len(self.anchors), -1)
         attention_features = torch.bmm(torch.transpose(batch_anchor_features, 1, 2),
                                        torch.transpose(attention_matrix, 1, 2)).transpose(1, 2)
